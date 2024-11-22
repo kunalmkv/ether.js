dcc04b062b74cba82291fc12163729944f0eac27
kunalmkv <kunalm4.kvtagore@gmail.com>
2024-11-22 16:04:31 +0530

    ethers | stable | write sc

--- readSC.js
+++ readSC.js
@@ -5,6 +5,8 @@ async function readContract() {
   const provider = new ethers.JsonRpcProvider('https://ethereum-sepolia-rpc.publicnode.com');
   const contractAddress = '0x9a26273805758299C6ee5205060aDa3BF085391D';
   const contract = new ethers.Contract(contractAddress, abi, provider);
+  const value = await contract.read();
+  console.log(value, "value")
   console.log(contract);
 }
 
